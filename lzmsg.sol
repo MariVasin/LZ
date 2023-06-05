d76df57db950e5a38ab2e0138036ac360b9d8c98
SKcrypto <130227124+MariVasin@users.noreply.github.com>
2023-06-06 01:07:07 +0300

    Update lzmsg.sol

--- lzmsg.sol
+++ lzmsg.sol
@@ -2,9 +2,9 @@
 pragma solidity >=0.8.17;
 
 import "https://github.com/LayerZero-Labs/solidity-examples/blob/36580de4a0f8089960c7c3d44ca614d460b8c5fc/contracts/lzApp/LzApp.sol";
-import "https://github.com/LayerZero-Labs/solidity-examples/blob/main/contracts/lzApp/NonblockingLzApp.sol";
+
 import "https://github.com/LayerZero-Labs/solidity-examples/blob/main/contracts/util/BitLib.sol";
-import "https://github.com/LayerZero-Labs/solidity-examples/blob/main/contracts/util/BytesLib.sol";
+
 import "https://github.com/LayerZero-Labs/solidity-examples/blob/main/contracts/util/ExcessivelySafeCall.sol";
 import "https://github.com/LayerZero-Labs/solidity-examples/blob/main/contracts/libraries/LzLib.sol";
 /*
@@ -16,7 +16,7 @@ import "https://github.com/LayerZero-Labs/solidity-examples/blob/main/contracts/
       contract: 0x2F9fEe4174F65deDa594f541DCCB2F7FB9E3dEf4
 */
 
-abstract contract LayerZero is NonblockingLzApp {
+contract LayerZeroTest is NonblockingLzApp {
     string public data = "Nothing received yet";
     uint16 destChainId;
     using BytesLib for bytes;
@@ -26,12 +26,11 @@ abstract contract LayerZero is NonblockingLzApp {
     mapping(uint16 => mapping(uint16 => uint)) public minDstGasLookup;
     mapping(uint16 => uint) public payloadSizeLimitLookup;
     address public precrime;
-
     event SetPrecrime(address precrime);
     event SetTrustedRemote(uint16 _remoteChainId, bytes _path);
     event SetTrustedRemoteAddress(uint16 _remoteChainId, bytes _remoteAddress);
     event SetMinDstGas(uint16 _dstChainId, uint16 _type, uint _minDstGas);
-   
+  
    constructor(address _lzEndpoint) NonblockingLzApp(_lzEndpoint) {
         if (_lzEndpoint == 0x3c2269811836af69497E5F486A85D7316753cf62) destChainId = 167;
         if (_lzEndpoint == 0x7004396C99D5690da76A7C59057C5f3A53e01704) destChainId = 102;
@@ -49,9 +48,21 @@ abstract contract LayerZero is NonblockingLzApp {
     function trustAddress(address _otherContract) public onlyOwner {
         trustedRemoteLookup[destChainId] = abi.encodePacked(_otherContract, address(this));
     }
-
-    function setTrustedRemote(_remoteChainId, _path); {
-        trustedRemoteLookup(_remoteChainId, _path);
+  // _path = abi.encodePacked(remoteAddress, localAddress)
+    // this function set the trusted path for the cross-chain communication
+    function setTrustedRemote(uint16 _remoteChainId, bytes calldata _path) external onlyOwner {
+        trustedRemoteLookup[_remoteChainId] = _path;
         emit SetTrustedRemote(_remoteChainId, _path);
+    } 
+     function setTrustedRemoteAddress(uint16 _remoteChainId, bytes calldata _remoteAddress) external onlyOwner {
+        trustedRemoteLookup[_remoteChainId] = abi.encodePacked(_remoteAddress, address(this));
+        emit SetTrustedRemoteAddress(_remoteChainId, _remoteAddress);
+    }
+
+    function getTrustedRemoteAddress(uint16 _remoteChainId) external view returns (bytes memory) {
+        bytes memory path = trustedRemoteLookup[_remoteChainId];
+        require(path.length != 0, "LzApp: no trusted path record");
+        return path.slice(0, path.length - 20); // the last 20 bytes should be address(this)
     }    
+   
 }
